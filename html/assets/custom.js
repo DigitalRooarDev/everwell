4e01742bd7d2cd0b2aca9e3f2542b98fbe24576e
Maulik-Suthar <94947583+Maulik-Suthar@users.noreply.github.com>
2023-12-07 12:39:13 +0530

    html added

---
+++ html/assets/custom.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".scrollTop").fadeIn();
+      $(".header-main").addClass("has_sticky");
+      $("body").addClass("sticky_header");
+    } else {
+      $(".scrollTop").fadeOut();
+      $(".header-main").removeClass("has_sticky");
+      $("body").removeClass("sticky_header");
+    }
+  });
+
+  $(window).scroll(function () {
+    var x = $(document).scrollTop();
+    if (x > 100) {
+      $(".back-to-top").addClass("active");
+    } else {
+      $(".back-to-top").removeClass("active");
+    }
+  });
+
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+
+  /* mobile-menu */
+  $(".menu-icon").click(function () {
+    $(this).toggleClass("menu-close");
+    $(".navigation-bar").toggleClass("slide-menu");
+    $("body").addClass("body-fixed");
+  });
+
+  $(".sidebar-overlay, .close-menu, .menu-main ul li a").click(function () {
+    $(".menu-icon").removeClass("menu-close");
+    $(".navigation-bar").removeClass("slide-menu");
+    $("body").removeClass("body-fixed");
+  });
+
+  $(".menu-main li:has(ul)").prepend('<span class="arrow"></span>');  
+  
+});
